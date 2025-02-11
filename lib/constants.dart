aa034222ebcb1b6fc37e0d0bc45de4b1a1d2061a
amrElzohairy <aelzohairy9@gmail.com>
2025-02-11 17:27:13 +0200

    Initialize Hive and add leading books constant
    - Added `kLeadingBooks` constant in `constants.dart` to store the name of the Hive box for leading books.
    - Modified `main.dart` to initialize Hive and register the `BookEntityAdapter`.
    - Ensured Flutter binding is initialized before running the app and opened the Hive box for leading books.

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
+const kLeadingBooks= 'LeadingBooks'; //Books
 // const kGtSectraFine = 'GT Sectra Fine';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:bookly_clean_arch/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const Bookly());
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kLeadingBooks);
 }
 
 class Bookly extends StatelessWidget {
