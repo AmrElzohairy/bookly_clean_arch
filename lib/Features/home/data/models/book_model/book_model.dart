67e61aa6e2c7e80a452ce1a78e9c3bf145b95c76
amrElzohairy <aelzohairy9@gmail.com>
2025-02-11 16:00:11 +0200

    Rename `items` to `bookData` in `BookModel` for clarity
    The `items` property in the `BookModel` class has been renamed to `bookData` to better reflect its purpose and improve code readability. This change affects the constructor, `fromJson` factory method, and `toJson` method. The J

--- lib/Features/home/data/models/book_model/book_model.dart
+++ lib/Features/home/data/models/book_model/book_model.dart
@@ -3,14 +3,14 @@ import 'book_data.dart';
 class BookModel {
   String? kind;
   int? totalItems;
-  List<BookData>? items;
+  List<BookData>? bookData;
 
-  BookModel({this.kind, this.totalItems, this.items});
+  BookModel({this.kind, this.totalItems, this.bookData});
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
         totalItems: json['totalItems'] as int?,
-        items: (json['items'] as List<dynamic>?)
+        bookData: (json['items'] as List<dynamic>?)
             ?.map((e) => BookData.fromJson(e as Map<String, dynamic>))
             .toList(),
       );
@@ -18,6 +18,6 @@ class BookModel {
   Map<String, dynamic> toJson() => {
         'kind': kind,
         'totalItems': totalItems,
-        'items': items?.map((e) => e.toJson()).toList(),
+        'items': bookData?.map((e) => e.toJson()).toList(),
       };
 }
