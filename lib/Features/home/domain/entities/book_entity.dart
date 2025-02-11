954cb6f3646f925cd9cd4e0711ca9c4e3b4a98f9
amrElzohairy <aelzohairy9@gmail.com>
2025-02-11 14:50:21 +0200

    Add BookEntity class and update dependencies
    Introduce the `BookEntity` class in the home feature domain to represent book data, including fields for image, title, author, rating, and pages. Additionally, update various transitive dependencies to their latest versions, ensuring compatibility and improved functionality. The Dart SDK version has also been updated to `>=3.7.0-0 <4.0.0`.

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity{
+  final String image;
+  final String title;
+  final String author;  
+  final num rating;
+  final num pages;
+
+  BookEntity({required this.image, required this.title, required this.author, required this.rating, required this.pages});
+}
