88998acf16a3fb327a17d281eb1f9f6a1c13cc45
amrElzohairy <aelzohairy9@gmail.com>
2025-02-11 14:29:35 +0200

    add bookly UI

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_clean_arch/core/utils/app_router.dart';
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+    );
+  }
+}
