a532c7c1fdbd316e3cc61bf54f85c815b06472d2
amrElzohairy <aelzohairy9@gmail.com>
2025-02-13 12:58:53 +0200

    Add fetchFeaturedBooks method to FeaturedBooksCubit
    Introduce the `fetchFeaturedBooks` method in the `FeaturedBooksCubit` to handle fetching featured books from the `HomeRepo`. The method uses the `getLeadingBooks` function from the repository and emits appropriate states (`FeaturedBooksFailure` or `FeaturedBooksSuccess`) based on the result. The `HomeRepo` is now a required parameter in the `FeaturedBooksCubit` constructor.

--- lib/Features/home/presentation/cubits/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_clean_arch/Features/home/domain/repo/home_repo.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/book_entity.dart';
@@ -6,5 +7,16 @@ import '../../../domain/entities/book_entity.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks() async {
+    var result = await homeRepo.getLeadingBooks();
+
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
